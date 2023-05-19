797b7da92446dc1365da3fe03069fa4a0878f3f0
Francisco Javier Perez <perezfrancisco@me.com>
2023-05-19 01:03:26 -0400

    Fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-#/bin/bash
+#!/bin/bash
 
-sudo cp ./lib/org.pansholoko.altportsshd.plist /Library/LaunchDeamons/
+sudo cp lib/org.pansholoko.altportsshd.plist /Library/LaunchDaemons/
+sudo cp lib/000-pansholoko.conf /etc/ssh/sshd_config.d/
 
-sudo launchctl load org.pansholoko.altportsshd
-sudo launchctl start org.pansholoko.altportsshd
+sudo launchctl load /Library/LaunchDaemons/org.pansholoko.altportsshd.plist
+sudo launchctl start /Library/LaunchDaemons/org.pansholoko.altportsshd.plist
